064d5d372b7a96319bfa66ca4aebc7012f2eab64
Ricardo Araujo <r_araujo_pereira@hotmail.com>
2020-05-02 17:29:29 -0300

    cardProfile do feed

--- online-demo-day/src/components/CardProfileSideBar/index.js
+++ online-demo-day/src/components/CardProfileSideBar/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import {ProfileSideBarWrapper} from "./style"
+import {ProfileSideBarWrapper, ProfilePicture, PictureWrapper,
+        LinksWrapper, LogoutWrapper, TextWrapper} from "./style"
+import Button from "@material-ui/core/Button";
+import NotificationIcon from '@material-ui/icons/AddAlarm';
+import MensageIcon from '@material-ui/icons/MailOutlineOutlined';
+import LikeIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import AccountIcon from '@material-ui/icons/AccountBox';
+
+
 
 
 class CardProfileSideBar extends React.Component{
@@ -10,7 +18,43 @@ class CardProfileSideBar extends React.Component{
 render(){
     return(
         <ProfileSideBarWrapper>
-            teste
+
+      
+                <PictureWrapper>
+                    <ProfilePicture src={require("../../images/image.png")}/>
+                    <p>Bill Gates</p>
+                </PictureWrapper>
+      
+
+            
+            <LinksWrapper>
+                <TextWrapper>
+                    <NotificationIcon/>
+                    <p>Notificações</p>
+                </TextWrapper>
+
+                <TextWrapper>
+                    <MensageIcon/>
+                    <p>Mensagens</p>
+                </TextWrapper>
+
+                <TextWrapper>
+                    <LikeIcon/>
+                    <p>Favoritos</p>
+                </TextWrapper>
+
+                <TextWrapper>
+                    <AccountIcon/>
+                    <p>Perfil</p>
+                </TextWrapper>
+            </LinksWrapper>
+            
+            <LogoutWrapper>
+                <Button variant="contained" color="primary">SAIR</Button>
+            </LogoutWrapper>
+
+
+
         </ProfileSideBarWrapper>
     )
 }
--- online-demo-day/src/components/CardProfileSideBar/style.js
+++ online-demo-day/src/components/CardProfileSideBar/style.js
@@ -1,6 +1,45 @@
 import styled from "styled-components";
 
 export const ProfileSideBarWrapper = styled.div`
-    
+    display:grid;
+    grid-template-rows: 3fr 4fr 1fr;
+    justify-items:center;
+`
 
+
+export const PictureWrapper = styled.div`
+    border-top: 1px solid white;
+    border-bottom: 1px solid white; 
+    width: 90%;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ProfilePicture = styled.img`
+    height:184px;
+    width:184px;
+    border-radius: 50%;
+    padding: 10px;
+`
+
+export const LinksWrapper = styled.div`
+    display: flex;
+    width: 90%;
+    flex-direction: column;
+    text-align: left;
+`
+
+export const TextWrapper = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+export const LogoutWrapper = styled.div`
+    border-top: 1px solid white;
+    width: 90%;
+    display:flex;
+    justify-content: center;
+    align-items: center;
 `
